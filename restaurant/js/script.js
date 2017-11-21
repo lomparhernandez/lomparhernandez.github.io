b565d4082262431d8e1c8c96dd575bf94111bf62
Marko <32530361+lomparhernandez@users.noreply.github.com>
2017-11-21 01:07:37 -0400

    comment

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -11,3 +11,16 @@ function scrolla() {
 		document.getElementById("topnav").style.height = "10vh";
   }
 }
+
+//Google mapping
+//or at least trying
+
+/**
+function juanitosLoc() {
+		var mapLoc = {
+		center : new google.maps.LatLng(37.8005012,-122.4025444),
+		zoom : 14
+		};
+	var map = new google.maps.Map(document.getElementById("locs"),mapLoc);
+}
+**/
