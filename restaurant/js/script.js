366bdc810f72f5cf8537f0a7f851897ea7b0574b
Marko <32530361+lomparhernandez@users.noreply.github.com>
2019-06-01 16:37:50 +0000

    Update script.js

--- restaurant/js/script.js
+++ restaurant/js/script.js
@@ -13,6 +13,32 @@ function scrolla() {
   }
 }
 
+$(document).ready(function(){
+  
+  $('.dis, .men, .eve, .vis').click(function() {
+    
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      
+      var $target = $(this.hash);
+      
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 500);
+       return false;
+        
+      }
+      
+    }
+    
+  });
+  
+});
+
 //Google mapping
 //or at least trying
 
