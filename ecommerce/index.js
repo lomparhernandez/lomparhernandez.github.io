964c189009e3bd72e73bcb6a223c6330b1d95eb5
Marko <32530361+lomparhernandez@users.noreply.github.com>
2019-06-07 02:03:54 +0400

    Create index.js
    init

---
+++ ecommerce/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MainApp extends React.Component {
+  render() {
+    return <h1>ECOM Store</h1>;
+  }
+}
+
+ReactDOM.render(<MainApp />, document.getElementById("main"));
